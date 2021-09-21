25436cb4ffb349e913bb3ebc878ae65b07ffc02b
Vitalii-Vishnyakov <vitalii_vish@mail.ru>
2021-09-21 22:58:44 +0300

    add Fail Card and android shadow

--- src/screens/AddFail.js
+++ src/screens/AddFail.js
@@ -2,38 +2,37 @@ import React from 'react';
 import { Text, View, StyleSheet } from 'react-native';
 import { FlatList } from 'react-native-gesture-handler';
 
+import { Dimensions } from 'react-native';
 export const AddFail = ({}) => {
-  const DATA = [
-    '1',
-    '2',
-    '3',
-    '4',
-    '5',
-    '6',
-    'gf',
-    'g',
-    's',
-    'sd',
-    'f',
-    'd',
-    'st',
-    'dt',
-  ];
+  const DATA = ['1', '2', '3', '4', '5'];
   return (
     <View style={styles.center}>
       <FlatList
         data={DATA}
         renderItem={({ item }) => (
-          <Text style={styles.text}>{item}</Text>
+          <View style={styles.wrapper}>
+            <View style={styles.block}>
+              <Text>{item}</Text>
+            </View>
+          </View>
         )}
         keyExtractor={(item) => item}
         horizontal={true}
         inverted
+        showsHorizontalScrollIndicator={false}
       ></FlatList>
     </View>
   );
 };
 const styles = StyleSheet.create({
-  center: { padding: 20 },
-  text: { fontSize: 37, padding: 40 },
+  center: { padding: 20, paddingRight: 30 },
+  wrapper: { padding: 10 },
+  block: {
+    width: Dimensions.get('window').width / 1.3,
+    height: Dimensions.get('window').height / 1.2,
+
+    borderRadius: 25,
+    backgroundColor: 'rgba(200, 200, 255, 0.3)',
+    elevation: 4,
+  },
 });
